3b6a3c71b8bf37f04de2490bafff5f9b5505b37f
John Silver <Sergey081203@ya.ru>
2024-02-19 01:10:03 +0300

    chore: Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name="silvergram",
     version="0.0.1",
     packages=find_packages(),
     description="A convenient shell for the aiogram library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="SilverBullet Technologies",
     author_email="tech.silver.bullet@gmail.com",
     url="https://github.com/yourusername/mathlib",
-    install_requires=[
-        "aiogram>=3.4.1"
-    ]
+    install_requires=install_requires
 )
+
