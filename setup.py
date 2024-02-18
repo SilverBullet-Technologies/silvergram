da196680ebfa937864cdc0dcb609332b6cc5d30c
John Silver <Sergey081203@ya.ru>
2024-02-19 01:28:06 +0300

    chore: Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="silvergram",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     description="A convenient shell for the aiogram library",
     long_description=open("README.md").read(),
