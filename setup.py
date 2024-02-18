82c0646a2550c1b1a180942257827691dc09f817
John Silver <Sergey081203@ya.ru>
2024-02-19 01:10:43 +0300

    chore: Updated setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description_content_type="text/markdown",
     author="SilverBullet Technologies",
     author_email="tech.silver.bullet@gmail.com",
-    url="https://github.com/yourusername/mathlib",
+    url="https://github.com/SilverBullet-Technologies/silvergram",
     install_requires=install_requires
 )
 
